7651db5e13c5a32026db41109f19d81143d3a5e4
nafi durmus <nafidurmus07@gmail.com>
2017-12-07 22:53:36 +0300

    keyboard events

---
+++ 1.gün/keyboard_events/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#vue-app',  //buradaki girdiğimiz değer index.html deki id değeriyle aynı olmalı.
+    data: {
+   
+    },
+    methods: {
+    	isimGir: function(){
+            console.log('ismini gir')
+
+        },
+        yasGir: function(){
+            console.log('yaşını gir')
+
+        }
+    	
+    }
+});
+
