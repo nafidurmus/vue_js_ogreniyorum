8396ab4a14be5d154032c06829f55368599a7f67
nafi durmus <nafidurmus07@gmail.com>
2017-12-09 14:12:11 +0300

    looping with v-for

---
+++ 3.gün/looping_with_v-for/app.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#vue-app',  
+    data: {
+        //name: 'nafi',
+        characters: ['mario','luigi','yoshi','bowser'],
+        ninjas:[
+            {name:'Ryu',age:25},
+            {name:'yoshi',age:35},
+            {name:'ken',age:55}
+        ]
+        
+   
+    },
+    methods: {
+      
+    	
+    	
+    },
+    computed:{ 
+     
+        
+    }
+});
+
